b64eb83228e1a4ec905baba38acd052ec69fe152
shawonahme <shawonahmed55660@gmail.com>
2024-10-02 22:01:14 +0600

    first navbar and category section add

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function loadData(){
-    
+    fetch  ("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then(res => res.json())
+    .then(data => displayData(data.categories))
 }
+
+
+function displayData(catagory){
+    const mycataContainer = document.getElementById('catagroy_container')
+
+  catagory.forEach(showData => {
+    
+    const cataBtn = document.createElement('button');
+    cataBtn.classList ='btn  hover:bg-red-600   hover:text-white'
+   
+    cataBtn.innerText = showData.category;
+    
+    mycataContainer.appendChild(cataBtn)
+
+  });
+
+}
+loadData()
